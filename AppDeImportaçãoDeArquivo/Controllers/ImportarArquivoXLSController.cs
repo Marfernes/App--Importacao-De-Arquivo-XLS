5bcb6076ad1f1139f2aa01a9c81f3ec61b2c8904
Marfernes <fernandesnunes026@gmail.com>
2021-11-09 13:51:32 -0400

    craiado a controller de importarArquivoXLS

---
+++ AppDeImportaçãoDeArquivo/Controllers/ImportarArquivoXLSController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AppDeImportaçãoDeArquivo.Controllers
+{
+    public class ImportarArquivoXLSController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult ImportarArquivoXLS()
+        {
+            return View();
+        }
+    }
+}
