220286a96c101f77682fa462350f3fc2ac9f7ab0
Marfernes <fernandesnunes026@gmail.com>
2021-11-09 14:16:56 -0400

    foi criado a interface da repository e a repository

---
+++ ImportarArquivoXLS.Domain/Interfaces/Repository/IRepositoryBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImportarArquivoXLS.Domain.Interfaces
+{
+    public interface IRepositoryBase<TEntity> where TEntity : class
+    {
+        void Adicionar(TEntity entity);
+        void Atualizar(TEntity entity);
+        void Remover(TEntity entity);
+        TEntity BuscarPorId(Guid Id);
+        IEnumerable<TEntity> BuscarTodos();
+    }
+}
---
+++ ImportarArquivoXLS.Infra/Repository/RepositoryBase.cs
@@ -0,0 +1,35 @@
+﻿using ImportarArquivoXLS.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImportarArquivoXLS.Infra.Repository
+{
+    public class RepositoryBase<TEntity> : IRepositoryBase<TEntity> where TEntity : class
+    {
+        public void Adicionar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity BuscarPorId(Guid Id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remover(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
