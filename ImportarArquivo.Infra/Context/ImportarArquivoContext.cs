6584c0007ff632fb660221867a1ebf9bf78d4a84
Marfernes <fernandesnunes026@gmail.com>
2021-11-09 10:13:07 -0400

    implementação do context na pasta Context

---
+++ ImportarArquivo.Infra/Context/ImportarArquivoContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ImportarArquivo.Infra.Context
+{
+    public class ImportarArquivoContext : DbContext
+    {
+        public IConfiguration _iconfiguration;
+        protected ImportarArquivoContext(DbContextOptions<ImportarArquivoContext> options, IConfiguration configuration) : base(options)
+        {
+            _iconfiguration = configuration;
+        }
+
+    }
+}
