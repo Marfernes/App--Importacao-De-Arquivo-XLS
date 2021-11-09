c3f4290c7a8f87b51bb97329baaf0d0b62b4475f
Marfernes <fernandesnunes026@gmail.com>
2021-11-09 14:05:44 -0400

    Foi criado a Interface e Service do sistema

---
+++ ImportarArquivoService/Interfaces/IServiceBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImportarArquivoService.Interfaces
+{
+    public interface IServiceBase<TEntity> where TEntity:class
+    {
+        void Adicionar(TEntity entity);
+        void Atualizar(TEntity entity);
+        void Excluir(TEntity entity);
+        TEntity BuscarPorId(Guid ID);
+        IEnumerable<TEntity> BuscarTodos();
+    }
+}
---
+++ ImportarArquivoService/Services/ServiceBase.cs
@@ -0,0 +1,35 @@
+﻿using ImportarArquivoService.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ImportarArquivoService.Services
+{
+    public class ServiceBase<TEntity> : IServiceBase<TEntity> where TEntity : class
+    {
+        public void Adicionar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Atualizar(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public TEntity BuscarPorId(Guid ID)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<TEntity> BuscarTodos()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Excluir(TEntity entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
