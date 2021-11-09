3cc4098145938a4cfc601d8b2806173eda5136ca
Marfernes <fernandesnunes026@gmail.com>
2021-11-09 09:11:48 -0400

    Iniciando Projeto

---
+++ AppDeImportaçãoDeArquivo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace AppDeImportaçãoDeArquivo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
